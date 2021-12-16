649b0be0dd441ea9fcccad4fea8d75f2c02f57ff
WellsRC <chad.richard.wells@gmail.com>
2021-12-16 10:31:32 -0500

    Organized files

--- Hellewel et al/PCRSens.m
+++
@@ -1,9 +0,0 @@
-function p = PCRSens(t,beta)
-%  V = ViralShedding_Symptomatic(t,tL,inf);
-%   p= V.^beta(1)./(V.^beta(1)+beta(2));
-
-mmm=exp(beta(1)-beta(2)^2);
-% mmm=(beta(1)-1).*beta(2);
-p=beta(3).*lognpdf(t,beta(1),beta(2))./lognpdf(mmm,beta(1),beta(2));
-end
-
