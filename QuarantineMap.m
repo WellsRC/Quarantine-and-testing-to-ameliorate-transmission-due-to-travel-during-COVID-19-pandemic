12b59ac47c3dd54d0f6a6916a6f8459f55c0b7e4
WellsRC <chad.richard.wells@gmail.com>
2021-12-17 08:49:32 -0500

    update

--- QuarantineMap.m
+++ QuarantineMap.m
@@ -37,8 +37,8 @@ for ii=1:length(Country)
         geoshow(X(ts), 'FaceColor', cmap(QM(ii)+1,:),'LineWidth',1.5)
     end
 end
-text(mean([-0.1886    0.56]),1.5848,CountryS,'Fontsize',20,'HorizontalAlignment','center');
-set(gca,'Xlim',[-0.1886    0.56],'Ylim',[0.62 1.55],'visible','off');
-text(-0.2475,1.5848,char(64+splot),'Fontsize',32,'FontWeight','bold')
+text(mean([-0.1386    0.61]),1.5848,CountryS,'Fontsize',20,'HorizontalAlignment','center');
+set(gca,'Xlim',[-0.1386    0.61],'Ylim',[0.62 1.55],'visible','off');
+text(-0.1983370,1.5848,char(64+splot),'Fontsize',32,'FontWeight','bold')
 end
 
